de939cc2778d1b8961e97cb682d99827667cd995
RudhraD <rudhra.duraisamy@rootquotient.com>
2025-10-23 01:07:45 +0530

    Added log for Feign Client

--- src/main/java/com/example/demo/client/SendGridClient.java
+++ src/main/java/com/example/demo/client/SendGridClient.java
@@ -1,13 +1,19 @@
 package com.example.demo.client;
 
 
+import com.example.demo.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "sendgridClient", url = "https://api.sendgrid.com/v3")
+@FeignClient(
+        name = "sendGridClient",
+        url = "https://api.sendgrid.com/v3",
+        configuration = FeignConfig.class
+)
+
 public interface SendGridClient {
 
     @PostMapping(value = "/mail/send", consumes = MediaType.APPLICATION_JSON_VALUE)
